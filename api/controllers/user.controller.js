9548978a4aaa0203319348b6a5aba7b57cecf6f7
lokesh bhardwaj <l.bhardwaj95@gmail.com>
2023-10-23 22:34:30 +0530

    create get userlisting API route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
+import Listing from "../models/listing.model.js";
 
 export const test = (req, res) => {
   res.json({
@@ -35,19 +36,30 @@ export const updateUser = async (req, res, next) => {
   }
 };
 
-export const deleteUser = async(req, res, next) => {
-  if (req.user.id !== req.params.id) 
-    return next(errorHandler(401, "you can only update your own account."))
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "you can only update your own account."));
   try {
     await User.findByIdAndDelete(req.params.id);
 
-    res.clearCookie('access_token');
+    res.clearCookie("access_token");
 
-    res.status(200).json('user has been deleted');
-    console.log('delete customer data from db.');
-
+    res.status(200).json("user has been deleted");
+    console.log("delete customer data from db.");
   } catch (error) {
-
     next(error);
+  }
+};
+
+export const getUSerListing = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id }); 
+      res.status(200).json(listings); 
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "you can only view your own listings."));
   }
 };
